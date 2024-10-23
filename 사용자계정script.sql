4a1d91101ee6f1cf4391946f552fe2703163e76c
blackpearl467 <kim991005@naver.com>
2024-10-23 17:34:58 +0900

    10월23일

--- 사용자계정script.sql
+++ 사용자계정script.sql
@@ -14,13 +14,13 @@
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
 -- 11G 이전 문법 사용 허용
 
-CREATE USER workbook IDENTIFIED BY workbook;
+CREATE USER todoList_jdbc IDENTIFIED BY todoList1234;
 -- 계정 생성 구문 (kh_kbg : id / kh1234 : pw)
 
-GRANT RESOURCE, CONNECT TO workbook;
+GRANT RESOURCE, CONNECT TO todoList_jdbc;
 -- 사용자 계정 권한 부여 설정
 -- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
 -- CONNECT : DB에 연결하고 로그인할 수 있는 권한
 
-ALTER USER workbook DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER todoList_jdbc DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
 -- 객체가 생성될 수 있는 공간 할당량 무제한 지정
